a8bc4883c225185decaf4cc0fe3da519e9759bfb
Daniel Fitzpatrick <doubleagent03@gmail.com>
2012-04-24 23:47:53 -0500

    updating project, since i forgot on the previous commit

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.3.3"]
-                 [clj-time "0.3.7"]])
+                 [clj-time "0.3.7"]
+                 [org.clojure/data.csv "0.1.2"]])
