648237f5ca1068b38dd6d9d2c9a6a8c79355429e
Daniel Fitzpatrick <doubleagent03@gmail.com>
2016-01-18 20:53:49 -0600

    Removed some hanging new lines

--- src/stocker/core.clj
+++ src/stocker/core.clj
@@ -7,7 +7,7 @@
 (def curr-url "http://finance.yahoo.com/d/quotes.csv")
 (def past-url "http://ichart.yahoo.com/table.csv")
 (def stats-map {
-     :price "l1" 
+     :price "l1"
      :change "c1"
      :vol "v"
      :avg-daily-vol "a2"
@@ -35,16 +35,16 @@
          (reverse coll)
          coll))
 
-(defn safekey 
+(defn safekey
   "Given a keyword name, replaces all spaces with a dash, 
    transforms it to lowercase, and then makes it a keyword"
-  [name] 
-  (-> name 
-      (cstr/replace #" " "-") 
-      (cstr/lower-case) 
+  [name]
+  (-> name
+      (cstr/replace #" " "-")
+      (cstr/lower-case)
       (keyword)))
 
-(defn build-map 
+(defn build-map
   "[ks vs] -> Given these two collections, constructs a hashmap of {ks1 vs1 ks2 vs2 ...}"
   [ks vs] (apply hash-map (interleave ks vs)))
 
@@ -63,8 +63,8 @@
   ([sym & stats]
    (->> (apply curr-stats-raw sym stats)
         (csv/read-csv)
-        ;; For some reason, yahoo reorders stats after 9 
-        ;; entries, so we need to reverse the stats if 
+        ;; For some reason, yahoo reorders stats after 9
+        ;; entries, so we need to reverse the stats if
         ;; less than 9 were requested, and not touch it
         ;; if more than 9 were requested.  Need to cons
         ;; 9 onto the form, since read-csv returns a nested
@@ -79,19 +79,19 @@
   "[sym start end] -> returns the historical data for this stock between start & end, inclusive (raw)
    [sym interval] -> returns the historical data from this stock within interval, inclusive (raw)"
   ([sym start end]
-   (let [url (str past-url "?s=" sym 
-                  "&a=" (ctime/month start) "&b=" (ctime/day start) "&c=" (ctime/year start) 
-                  "&d=" (ctime/month end) "&e=" (ctime/day end) "&f=" (ctime/year end) 
+   (let [url (str past-url "?s=" sym
+                  "&a=" (ctime/month start) "&b=" (ctime/day start) "&c=" (ctime/year start)
+                  "&d=" (ctime/month end) "&e=" (ctime/day end) "&f=" (ctime/year end)
                   "&g=d&ignore=.csv")]
      (:body (client/get url))))
   ([sym interval] (hist-stats-raw sym (ctime/start interval) (ctime/end interval))))
 
-(defn hist-stats 
+(defn hist-stats
   "[sym start end] -> returns the historical data for this stock between start & end, inclusive
    [sym interval] -> returns the historical data from this stock within interval, inclusive"
-  ([sym start end] 
-   (let [stats (csv/read-csv (hist-stats-raw sym start end)) 
-         ks (apply map safekey (take 1 stats)) 
-         hm (partial build-map ks)] 
+  ([sym start end]
+   (let [stats (csv/read-csv (hist-stats-raw sym start end))
+         ks (apply map safekey (take 1 stats))
+         hm (partial build-map ks)]
      (vec (map hm (drop 1 stats)))))
   ([sym interval] (hist-stats sym (ctime/start interval) (ctime/end interval))))
